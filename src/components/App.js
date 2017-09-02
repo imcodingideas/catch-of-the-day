f04ea26b1adfe97519c7331c6b3cdf044064a982
Joseph Chambers <joseph@michael-chambers.com>
2017-09-01 22:45:08 -0400

    Latest

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes'
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    this.sampleFishes = this.sampleFishes.bind(this);
+    
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+
+    this.setState({ fishes });
+  }
+
+  loadSamples() {
+      this.setState({
+        fishes: sampleFishes
+      });
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish}/>
+      </div>
+    )
+  }
+}
+
+export default App;
